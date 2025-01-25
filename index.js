574e63c86d26838b60ac2ede032049eb1ad65c00
AsadEducation <AsadEducation@outlook.com>
2025-01-26 02:40:40 +0600

    all meals api updated for search and filter query

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, serialize } = require('mongodb');
 
 // middleware
 app.use(cors());
@@ -38,7 +38,38 @@ async function run() {
         //api for getting all meals
         app.get('/meals', async (req, res) => {
 
-            const result = await mealCollection.find().toArray();
+            // const sorted = req.query?.sorted;
+            // const search = req.query?.search; //console.log(search)
+            // const category = req.query?.category; console.log(category)
+
+            const { search, category, min, max } = req.query;
+
+            // let sortQuery = {};
+            // if (sorted == 'true') {
+            //     sortQuery = { expired_datetime: -1 }  //change expired_datetime 
+            // }
+
+            let searchQuery = {};
+
+            if (search) {
+                searchQuery = { name: { $regex: new RegExp(search, 'i') } };
+            }
+
+            if (category) {
+                searchQuery.category = category;
+            }
+
+            if (min || max) {
+
+                searchQuery.price = {};
+
+                if (min) searchQuery.price.$gte = parseFloat(min);
+                if (max) searchQuery.price.$lte = parseFloat(max);
+            }
+
+            const cursor = mealCollection.find(searchQuery).sort();
+
+            const result = await cursor.toArray(); console.log(result);
 
             res.send(result);
         })
@@ -62,7 +93,7 @@ async function run() {
 
             const id = req.params.id;
 
-            const reactionCount = req.body.reactionCount;
+            const reactionCount = await req.body.reactionCount;
 
             // console.log(req.body);
 
@@ -72,7 +103,7 @@ async function run() {
 
             const updateDoc = {
                 $set: {
-                    reactionCount: reactionCount
+                    reactionCount
                 }
             }
 
